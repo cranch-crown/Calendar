6c177ba1af8cd3b0b1f37fcda92857df6acd3a51
Yohei Maeshima <ymzlgcranch@gmail.com>
2018-12-09 17:27:56 +0900

    バージョン1.0.3

---
+++ app/Http/Controllers/DemoUser.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+
+class DemoUser extends Controller
+{
+  public function createDemouser() {
+    $id = DB::table('users')->insertGetId([
+      'name' => 'デモユーザー',
+      'email' => uniqid().'@test.com',
+      'password' => bcrypt(uniqid()),
+      'created_at' => Carbon::now(),
+      'updated_at' => Carbon::now()
+    ]);
+    Auth::loginUsingId($id);
+    return redirect('/');
+  }
+}
