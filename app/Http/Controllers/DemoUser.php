a734a3e71c2cefdcc425f169c89f5fba8141af08
Yohei Maeshima <ymzlgcranch@gmail.com>
2018-12-09 16:54:55 +0900

    デモユーザー登録機能を改良

--- app/Http/Controllers/DemoUser.php
+++ app/Http/Controllers/DemoUser.php
@@ -5,16 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
-use App\User;
+/*use App\User;*/
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class DemoUser extends Controller
 {
   public function createDemouser() {
-    $users = new User;
+    $id = DB::table('users')->insertGetId([
+      'name' => 'デモユーザー',
+      'email' => Carbon::now()->timestamp.'@test.com',
+      'password' => bcrypt(Carbon::now()->timestamp)
+    ]);
+    /*$users = new User;
     $users->name = 'デモユーザー';
     $users->email = Carbon::now()->timestamp.'@test.com';
     $users->password = Carbon::now()->timestamp;
-    $users->save();
+    $users->save();*/
     return redirect('/');
   }
 }
